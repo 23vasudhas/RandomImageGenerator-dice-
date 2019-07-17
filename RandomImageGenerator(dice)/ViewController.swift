d6d7791ddc293f5c8f6e82bd26f7b1b7b5694dfd
Vasudha Subramanian <subramanian.vasudha@gmail.com>
2019-07-17 11:47:44 -0700

    Initial Commit

---
+++ RandomImageGenerator(dice)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomImageGenerator(dice)
+//
+//  Created by 23VasudhaSubramanian on 7/17/19.
+//  Copyright © 2019 Vasudha Subramanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
