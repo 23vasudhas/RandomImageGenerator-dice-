e7337a47a857a1d8b8f9a6a5dd1d43bbd23aa271
Vasudha Subramanian <subramanian.vasudha@gmail.com>
2019-07-17 17:20:52 -0700

    launch screen complete, basic rolling dice complete

--- RandomImageGenerator(dice)/ViewController.swift
+++ RandomImageGenerator(dice)/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    var diceNumbers = ["Dice Blue 1", "Dice Blue 2", "Dice Blue 3", "Dice Blue 4", "Dice Blue 5", "Dice Blue 6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        let randNum = Int.random(in: 0...5)
+        let randNum2 = Int.random(in: 0...5)
+        dice1.image =  UIImage(named :diceNumbers[randNum])
+        dice2.image = UIImage(named :diceNumbers[randNum2])
+    }
+    
 }
 
